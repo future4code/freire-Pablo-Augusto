3e4798008d55f884ec2468c9942414b28deefd56
Pablo Leão <pablo_leao@hotmail.com>
2022-06-23 20:28:25 -0300

    Projeto Labefy - Commit Inicial

---
+++ modulo2/projeto-labefy/labefy/src/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  document.title = 'Home - Labefy'
+  return (
+    <div className="App">
+      
+    </div>
+  );
+}
+
+export default App;
