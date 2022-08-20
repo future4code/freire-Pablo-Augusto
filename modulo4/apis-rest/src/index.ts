a5b1513145b5660e8945c09278263be2d9de0749
Pablo Leão <pablo_leao@hotmail.com>
2022-08-19 23:42:23 -0300

    Exercício 1 adicionado

--- modulo4/apis-rest/src/index.ts
+++ modulo4/apis-rest/src/index.ts
@@ -1,5 +1,20 @@
-import express from "express";
+import express from 'express';
+import cors from 'cors';
+import { User, users } from './users';
 
 const app = express();
-
 app.use(express.json());
+app.use(cors());
+const cyan = '\u001b[36m';
+
+// Exercício 1
+// a) O método .get()
+// b) /users
+app.get('/users', (req, res) => {
+    res.status(200).send(users);
+})
+
+
+app.listen(3003, () => {
+    console.log(cyan+"Server is running at http://localhost:3003");
+})
