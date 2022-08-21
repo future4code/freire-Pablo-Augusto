188538ac1f317397866c120addb52ac94177546a
Pablo Leão <pablo_leao@hotmail.com>
2022-08-21 03:28:52 -0300

    Requisitos 1 a 4 adicionados

--- modulo4/projeto-labebank/src/index.ts
+++ modulo4/projeto-labebank/src/index.ts
@@ -1,7 +1,24 @@
 import express from 'express';
 import cors from 'cors';
+import { Transacao, contas } from './contas';
 
-const app = express()
-app.use(express.json())
-app.use(cors())
-const cyan = '\u001b[36m'
+const app = express();
+app.use(express.json());
+app.use(cors());
+const cyan = '\u001b[36m';
+
+function calculaSaldo(transacoes?: Transacao[]): number {
+    let resultado: number = 0;
+
+    if (transacoes) {
+        for (let transacao of transacoes) {
+            resultado += transacao.valor
+        }
+    }
+
+    return resultado;
+}
+
+app.listen(3003, () => {
+    console.log(cyan+"Server running at http://localhost:3003");
+});
