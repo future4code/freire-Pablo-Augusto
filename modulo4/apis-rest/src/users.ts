ccc9e4eac930c44251b19c7503f47317490db614
Pablo Leão <pablo_leao@hotmail.com>
2022-08-21 02:25:09 -0300

    Exercícios restantes adicionados

--- modulo4/apis-rest/src/users.ts
+++ modulo4/apis-rest/src/users.ts
@@ -1,8 +1,13 @@
+export enum UserType {
+    ADMIN = "ADMIN",
+    NORMAL = "NORMAL"
+}
+
 export type User = {
     id: number,
     name: string,
     email: string,
-    type: "ADMIN" | "NORMAL",
+    type: UserType,
     age: number
 }
 
@@ -11,42 +16,42 @@ export const users: Array<User> = [
         id: 1,
         name: "Alice",
         email: "alice@email.com",
-        type: "ADMIN",
+        type: UserType.ADMIN,
         age: 12
     },
     {
         id: 2,
         name: "Bob",
         email: "bob@email.com",
-        type: "NORMAL",
+        type: UserType.NORMAL,
         age: 36
     },
     {
         id: 3,
         name: "Coragem",
         email: "coragem@email.com",
-        type: "NORMAL",
+        type: UserType.NORMAL,
         age: 21
     },
     {
         id: 4,
         name: "Dory",
         email: "dory@email.com",
-        type: "NORMAL",
+        type: UserType.NORMAL,
         age: 17
     },
     {
         id: 5,
         name: "Elsa",
         email: "elsa@email.com",
-        type: "ADMIN",
+        type: UserType.ADMIN,
         age: 17
     },
     {
         id: 6,
         name: "Fred",
         email: "fred@email.com",
-        type: "ADMIN",
+        type: UserType.ADMIN,
         age: 60
     }
 ]
