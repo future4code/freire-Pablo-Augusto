89fbd57bea32816749af9c41894130f4e82788d3
Pablo Leão <pablo_leao@hotmail.com>
2022-08-22 02:24:20 -0300

    Desafio 5 implementado

--- modulo4/projeto-labebank/src/index.ts
+++ modulo4/projeto-labebank/src/index.ts
@@ -96,6 +96,65 @@ app.post('/users', (req, res) => {
 
 });
 
+app.post('/users/:cpf/pagamento', (req, res) => {
+    const cpf = req.params.cpf;
+    const indiceConta = contas.findIndex(conta => conta.cpf === cpf);
+    const { dataVencimento, descricao, valor } = req.body;
+
+    try {
+        if (indiceConta < 0) {
+            throw new Error("Não existe um cliente cadastrado com esse CPF.");
+        }
+
+        if (!descricao || !valor) {
+            throw new Error("Algum valor não foi preenchido. Verifique os dados de descricao ou valor.");
+        }
+
+        if (isNaN(valor)) {
+            throw new Error("A propriedade valor deve ser do tipo number.");
+        }
+
+        if (valor <= 0) {
+            throw new Error("A propriedade valor deve ser maior que 0.")
+        }
+
+        let dataFormatada: number;
+        if (!dataVencimento) {
+            dataFormatada = Date.now();
+        } else {
+            dataFormatada = formataData(dataVencimento);
+        }
+
+        const transacao: Transacao = {
+            valor: (-1) * valor,
+            data: dataFormatada,
+            descricao: descricao
+        }
+
+        contas[indiceConta].extrato.push(transacao);
+        res.status(201).send("Pagamento realizado.");
+
+    } catch (error: any) {
+        switch (error.message) {
+            case "Algum valor não foi preenchido. Verifique os dados de descricao ou valor.":
+                res.status(400).send({ mensagem: error.message });
+                break;
+            case "A propriedade valor deve ser do tipo number.":
+                res.status(422).send({ mensagem: error.message });
+                break;
+            case "A propriedade valor deve ser maior que 0.":
+                res.status(422).send({ mensagem: error.message });
+                break;
+            case "Não existe um cliente cadastrado com esse CPF.":
+                res.status(404).send({ mensagem: error.message });
+                break;
+            default:
+                res.status(500).send("Erro inesperado!");
+                break;
+        }
+    }
+});
+
 app.put('/users/:cpf/deposito', (req, res) => {
     const cpf = req.params.cpf;
     const { nome, valor } = req.body;
@@ -107,6 +166,10 @@ app.put('/users/:cpf/deposito', (req, res) => {
 
         if (isNaN(valor)) {
             throw new Error("A propriedade valor deve ser do tipo number.");
+        }
+
+        if (valor <= 0) {
+            throw new Error("A propriedade valor deve ser maior que 0.")
         }
 
         const indiceConta = contas.findIndex(conta => conta.cpf === cpf && conta.nome.toLowerCase() === nome.toLowerCase());
@@ -128,6 +191,9 @@ app.put('/users/:cpf/deposito', (req, res) => {
                 res.status(400).send({ mensagem: error.message });
                 break;
             case "A propriedade valor deve ser do tipo number.":
+                res.status(422).send({ mensagem: error.message });
+                break;
+            case "A propriedade valor deve ser maior que 0.":
                 res.status(422).send({ mensagem: error.message });
                 break;
             case "Não existe um cliente cadastrado com esse CPF e nome":
